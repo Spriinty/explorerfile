545aed2a1c1fbc1dc0b921d14929b2b7bc1f2de7
Dev-hrod <rodrigue-cegelly@hotamil.com>
2019-11-25 15:08:35 +0000

    phase de test

--- traitement.php
+++ traitement.php
@@ -1,5 +1,18 @@
 <?php
 
+$chemindossier = "/archive/"
+
+// Ouvre un dossier bien connu, et liste tous les fichiers
+if (is_dir($chemindossier)) {
+    if ($dh = opendir($chemindossier)) {
+        while (($file = readdir($dh)) !== false) {
+            echo "fichier : $file : type : " . filetype($chemindossier . $file) . "\n";
+        }
+        closedir($dh);
+    }
+}
+
+}
 
 
 
