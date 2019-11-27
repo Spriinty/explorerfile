ece07225387a4b5ce46d761f84989e4f1b3e1bad
Spriinty <quentinpetrequin@gmail.com>
2019-11-27 15:51:17 +0000

    changement fonction idnex.php

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,6 +11,8 @@
 	 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.0/css/bootstrap.min.css" integrity="sha384-SI27wrMjH3ZZ89r4o+fGIJtnzkAnFs3E4qz9DIYioCQ5l9Rd/7UAa8DHcaL8jkWt" crossorigin="anonymous">
 	<link href="//fonts.googleapis.com/css?family=Lato:400,900" rel="stylesheet" type="text/css" />
 	<link rel="stylesheet" href="css/style.css">
+	
+
 </head>
 <body>
 <div class="container-fluid bg-dark mb-3">
@@ -17,7 +21,7 @@
 			<div class="col-3">Btn Home</a></div>
 			<div class="col-3"><a href="#">Flèche gauche</a></div>
 			<div class="col-3"><a href="#">Flèche droite</a></div>
-			<div class="col-3"><p><?php echo getcwd(); ?></p></div>
+			<div class="col-3 text-white"><p><?php echo getcwd(); ?></p></div>
 		</div>
 	</div>
 </div>
@@ -27,27 +31,48 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-12 text-center">
-
 			<?php
 
-			$path= "/var/www/html";
-			chdir($path);
+				chdir($path);
+				/* getcwd() gives you back "c:\temp\" */
+				// $ressource = opendir($path);
+			?>
+	
+			<?php
 
+			// $iterator = new DirectoryIterator($path);
 
-			/* getcwd() gives you back "c:\temp\" */
-			$ressource = opendir($path);
-			echo readdir($path);
-			
+			// while($iterator->valid()) {
+    		// 	$file = $iterator->current();
+    		// 	echo  $file->getFilename() ."<br>". "\n";
+    		// 	$iterator->next();
+			// }
 
+			/*--------------------------------*/
 
-			while(($entry = readdir($ressource)) !== FALSE){
-    			// if ($entry == '.' && $entry == '..'){
-				// 	echo "<a href=''>"
+			 // findfiles.php  -  what is in directory "videoarchive"
+			$path = getcwd()."/".$_GET["file"];
+			$dir = $path; // path from top
+			$files = scandir($dir);
+			$files_n = count($files);
+			var_dump($path);
+
+			$i=1;
+
+			while($i<=$files_n){
+				// "is_dir" only works from top directory, so append the $dir before the file
+				if (is_dir($dir.'/'.$files[$i])){
+
+					$MyFileType[$i] = "D" ; // D for Directory
+					echo '<br>'.$i.'. '. $MyFileType[$i].' . '.'<a href="index.php?file='.$files[$i].'">'.$files[$i].'</a>' ;
+				} else{
+					$MyFileType[$i] = "F" ; // F for File
+					echo '<br>'.$i.'. '. $MyFileType[$i].' . '.$files[$i] ;
+				}
+				// print itemNo, itemType(D/F) and itemname
 				
-				// }
-				echo $entry."<br/>";
+				$i++;
 			}
-
 			?>
 
 			</div>
